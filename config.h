eb1ba7c6d80fd70d6964502353c3eea85323ed01
oedotme <oedotme@gmail.com>
2021-05-18 13:08:15 +0200

    feat: update window border px margin

--- config.h
+++ config.h
@@ -12,7 +12,7 @@ static char *font2[] = {
 	"Noto Sans Symbols:size=12:antialias=true:autohint=true"
 };
 
-static int borderpx = 2;
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
